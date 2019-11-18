7a15b6379092c0360ea76eeb12b65b5fb72f367b
Qiu Jian <qiujian987@163.com>
2019-11-18 20:37:10 +0800

    path check before setting owner and group

--- puppet/modules/acl/lib/puppet/type/acl.rb
+++ puppet/modules/acl/lib/puppet/type/acl.rb
@@ -27,8 +27,14 @@ Puppet::Type.type(:acl).provide(:acler) do
     # notice "path=#{resource[:path]}"
     $owner_cur = nil
     $group_cur = nil
+    $path_exists = nil
+    $staus_msg = nil
 
     def get_perms
+        $path_exists = system "test -d #{resource[:path]}"
+        unless $path_exists
+            return
+        end
         facl = `getfacl #{resource[:path]}`.gsub('#','').split()
         fi = facl.index('file:')
         oi = facl.index('owner:')
@@ -40,16 +46,29 @@ Puppet::Type.type(:acl).provide(:acler) do
     def owner
         get_perms
         owner_def = resource[:owner]
+        $staus_msg = @resource.noop ? "should be" : "setting to be"
+        unless $path_exists
+            return $owner_cur
+        end        
         unless $owner_cur == owner_def
-            notice "changing owner current_value=#{$owner_cur}, should be #{owner_def}"
+            notice "changing owner current_value=#{$owner_cur}, #{$staus_msg} #{owner_def}"
+            unless @resource.noop
+                system "chown #{owner_def} #{resource[:path]}"
+            end
         end
         return owner_def
     end
 
     def group
         group_def = resource[:group]
+        unless $path_exists
+            return group_def
+        end        
         unless $group_cur == group_def
-            notice "changing group current_value=#{$group_cur}, should be #{group_def}"
+            notice "changing group current_value=#{$group_cur},#{$staus_msg} #{group_def}"
+            unless @resource.noop
+                system "chgrp #{group_def} #{resource[:path]}"
+            end
         end
         return group_def
     end    
