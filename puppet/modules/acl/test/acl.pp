bca63375d1c7a55c5a4a599b7fc8f8feab6c347b
Qiu Jian <qiujian987@163.com>
2019-11-21 20:24:17 +0800

    tag

--- puppet/modules/acl/test/acl.pp
+++ puppet/modules/acl/test/acl.pp
@@ -1,14 +1,19 @@
-$allpaths = lookup('acl::allpaths')
+class mytest{
+	tag 'hello'
+	$allpaths = lookup('acl::allpaths')
 
-$allpaths.each |Hash $path|{
-	hfile { $path['path']:
-		ensure => present
+	$allpaths.each |Hash $path|{
+		hfile { $path['path']:
+			ensure => present
+		}
+		->
+		acl {$path['path']:
+		    # mode => $path['mode'],
+		    owner => $path['owner'],
+		    group => $path['group'],
+		    # ensure => present
+		}	
 	}
-	->
-	acl {$path['path']:
-	    # mode => $path['mode'],
-	    owner => $path['owner'],
-	    group => $path['group'],
-	    # ensure => present
-	}	
+
 }
+
