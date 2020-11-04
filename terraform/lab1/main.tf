bb717848202418891a62c7e4f983df3e614843fa
Jian Li <jian.li.85@foxmail.com>
2020-11-04 10:27:25 +0800

    added pub sub

--- terraform/lab1/main.tf
+++ terraform/lab1/main.tf
@@ -1,5 +1,33 @@
 provider "google" {
-  project = "qwiklabs-gcp-01-26770522e2ca"
+  project = "qwiklabs-gcp-04-145910e5f794"
   region  = "us-central1"
   zone    = "us-central1-c"
+}
+
+resource "google_pubsub_topic" "example" {
+  name = "example-topic"
+}
+
+resource "google_pubsub_subscription" "example" {
+  name  = "example-subscription"
+  topic = google_pubsub_topic.example.name
+
+  labels = {
+    foo = "bar"
+  }
+
+  # 20 minutes
+  message_retention_duration = "1200s"
+  retain_acked_messages      = true
+
+  ack_deadline_seconds = 20
+
+  expiration_policy {
+    ttl = "300000.5s"
+  }
+  retry_policy {
+    minimum_backoff = "10s"
+  }
+
+  enable_message_ordering    = false
 }
